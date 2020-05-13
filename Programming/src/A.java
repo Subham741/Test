d658edbd73e05fe6b3ecca1306dd9bbfaca312ce
Subham741 <65287830+Subham741@users.noreply.github.com>
2020-05-13 17:28:56 +0530

    Update A.java

--- Programming/src/A.java
+++ Programming/src/A.java
@@ -6,7 +6,7 @@ import java.util.StringTokenizer;
  
 public class A {
     
-    public static void main (String[] args) throws Exception
+    public static void main (String[] args) throws java.lang.Exception
 	{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int t = Integer.parseInt(br.readLine().trim());
